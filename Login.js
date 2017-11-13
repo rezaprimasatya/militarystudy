ee2eb067484acdb258ab49cc865765e5c0977d18
dhgevandha <31717061+dhgevandha@users.noreply.github.com>
2017-11-13 09:57:37 +0800

    New Window Login

---
+++ Login.js
@@ -0,0 +1,5 @@
+//jendela baru
+document.getElementById(tombolLogIn).addEventListener("click",jendelaBaru);
+function jendelaBaru(){
+    newWindow = window.open("","jendela baru","width=300","height=200");
+}
